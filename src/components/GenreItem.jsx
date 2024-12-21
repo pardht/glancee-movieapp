c26a7df4d82459c88308c6457c31c9cce5b13fe6
ran <randyparadita31@gmail.com>
2024-12-21 21:34:22 +0800

    add more genres in navigation

--- src/components/GenreItem.jsx
+++ src/components/GenreItem.jsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 import React from 'react'
 import { useSearchParams } from 'next/navigation'
 
-export default function GenreItem({title, param}) {
+export default function GenreItem({title, param, bg}) {
     
     const searchParams = useSearchParams();
     const genre = searchParams.get('genre');
   return (
     <Link href={`/?genre=${param}`}>
-        <div className={`hover:text-amber-500 ${genre === param ? 'underline' : ''}`}>
+        <div className={`hover:bg-white/10 py-[12px] px-7 flex justify-center rounded-full text-white font-light text-sm ${genre === param ? 'bg-white/10' : ''}`}>
             <p>{title}</p>
         </div>
     </Link>
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
 
-export default function MenuItem({ title, address, Icon }) {
+export default function MenuItem({ title, address, Icon, size }) {
     return (
         <Link href={address}>
-            <div className='w-[90px] h-[45px] flex items-center justify-center rounded-full duration-200 text-white hover:bg-black/10'>
-                <p className='hidden md:block'>{title}</p>
-                <Icon className='md:hidden' />
+            <div className=' duration-200 hover:opacity-80 text-white bg-white/10 rounded-full p-3 flex items-center'>
+                <Icon size={size} className='' />
+                <p>{title}</p>
             </div>
         </Link>
     )
