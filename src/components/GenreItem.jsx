2e41771a595b4e95ce594081e2d7168f222e8e10
ran <randyparadita31@gmail.com>
2024-12-22 13:33:29 +0800

    add suspense to wrap component

--- src/components/GenreItem.jsx
+++ src/components/GenreItem.jsx
@@ -1,17 +1,19 @@
 "use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-export default function GenreItem({title, param, bg}) {
-    
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre');
+export default function GenreItem({ title, param, bg }) {
+
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
   return (
-    <Link href={`/?genre=${param}`}>
+    <Suspense>
+      <Link href={`/?genre=${param}`}>
         <div className={`hover:bg-white/10 py-[12px] px-7 flex justify-center rounded-full text-white font-light text-sm ${genre === param ? 'bg-white/10' : ''}`}>
-            <p>{title}</p>
+          <p>{title}</p>
         </div>
-    </Link>
+      </Link>
+    </Suspense>
   )
 }
