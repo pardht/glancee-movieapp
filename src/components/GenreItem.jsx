ec7289b60b55ce0655d9fc06ba4b69a478b1e81d
ran <randyparadita31@gmail.com>
2024-12-24 20:30:53 +0800

    new suspense layout

--- src/components/GenreItem.jsx
+++ src/components/GenreItem.jsx
@@ -3,17 +3,23 @@ import Link from 'next/link'
 import React, { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-export default function GenreItem({ title, param, bg }) {
+export default function GenreItem({ title, param }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GenreLink title={title} param={param} />
+    </Suspense>
+  );
+}
 
+function GenreLink({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get('genre');
+
   return (
-    <Suspense>
-      <Link href={`/?genre=${param}`}>
-        <div className={`hover:bg-white/10 py-[12px] px-7 flex justify-center rounded-full text-white font-light text-sm ${genre === param ? 'bg-white/10' : ''}`}>
-          <p>{title}</p>
-        </div>
-      </Link>
-    </Suspense>
-  )
+    <Link href={`/?genre=${param}`}>
+      <div className={`hover:bg-white/10 py-[12px] px-7 flex justify-center rounded-full text-white font-light text-sm ${genre === param ? 'bg-white/10' : ''}`}>
+        <p>{title}</p>
+      </div>
+    </Link>
+  );
 }
